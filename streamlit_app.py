d1d8401de5599167c9ff4430c6cae768f55f0b35
deeps <122277040+DeepaP01@users.noreply.github.com>
2023-01-09 18:53:04 +0530

    Update streamlit_app.py
    added fruits names

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,13 @@ streamlit.header( '\N{flexed biceps}Breakfast Menu\N{flexed biceps}')
 streamlit.text('🌶️ Omega 3 & Blueberry Oatmeal')
 streamlit.text('🍂Kale, Spinach and Rocket Smoothie')
 streamlit.text('🥚 Hard-Boiled Free Range Egg')
-streamlit.text('🥑 Hard-Boiled Free Range Egg')
+streamlit.text('🥑 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 #let put the list here
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
  #display the table here
 streamlit.dataframe(my_fruit_list)
 
