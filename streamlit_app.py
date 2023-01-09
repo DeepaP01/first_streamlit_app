7c4b0c04590788808548fa964c7a966436afe20f
deeps <122277040+DeepaP01@users.noreply.github.com>
2023-01-09 18:43:29 +0530

    Update streamlit_app.py
    corrected

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 
 import streamlit
-import pandas
+
 streamlit.title('my parents New healthy dinner')
 streamlit.header( '\N{flexed biceps}Breakfast Menu\N{flexed biceps}')
 streamlit.text('🌶️ Omega 3 & Blueberry Oatmeal')
@@ -8,5 +8,6 @@ streamlit.text('🍂Kale, Spinach and Rocket Smoothie')
 streamlit.text('🥚 Hard-Boiled Free Range Egg')
 streamlit.text('🥑 Hard-Boiled Free Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit_dataframe(my_fruit_list)
