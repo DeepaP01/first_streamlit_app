3caa077e2ed401764ff8260e061a8d60b8db92ba
deeps <122277040+DeepaP01@users.noreply.github.com>
2023-01-09 18:22:48 +0530

    Update streamlit_app.py
    added emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit
 streamlit.title('my parents New healthy dinner')
 streamlit.header( 'Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.text('Hard-Bolied Free Range Egg')
+streamlit.text('🌶️ Omega 3 & Blueberry Oatmeal')
+streamlit.text('🍂Kale, Spinach and Rocket Smoothie')
+streamlit.text('🥚 Hard-Boiled Free Range Egg')
+streamlit.text('🥑 Hard-Boiled Free Range Egg')
