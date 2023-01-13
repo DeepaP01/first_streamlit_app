3cd8e882b579c3eab6d6495c8ff93b108698e9e5
deeps <122277040+DeepaP01@users.noreply.github.com>
2023-01-13 17:03:51 +0530

    Update streamlit_app.py
    changed to apple

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,9 @@ streamlit.dataframe(fruits_to_show)
 import requests
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('what fruit would you like information about?', 'kiwi')
+fruit_choice = streamlit.text_input('what fruit would you like information about?', 'apple')
 streamlit.write('the user entered',fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "fruit_choice")
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
 
